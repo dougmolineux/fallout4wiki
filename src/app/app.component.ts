347568198208873c36a5fad26f88dd9d6cf30630
dougmolineux <doug.molineux@gmail.com>
2018-03-04 10:52:16 -0700

    Prettified the Achievements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,16 @@ import achievements from '../../data/achievements.json';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  achievements = achievements
+  namesOfTrophies = [];
+  achievements = achievements;
   title = 'Fallout 4 Wiki Angular Learning';
   ngOnInit() {
-    console.log("achievements", achievements);
+    console.log("this.achievements", this.achievements);
     this.achievements.splice(0, 10);
+    this.namesOfTrophies = this.achievements.filter( (value, index) => {
+      if(!index) return true;
+      return index % 6 === 0;
+    });
+    console.log("namesOfTrophies", this.namesOfTrophies.map( (t) => t.Field1 ));
   }
 }
