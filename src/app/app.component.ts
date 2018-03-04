d40e74d604eeba8978886682cdaf4e3531b2cb93
dougmolineux <doug.molineux@gmail.com>
2018-03-03 22:45:25 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import achievements from '../../data/achievements.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  achievements = achievements
+  title = 'Fallout 4 Wiki Angular Learning';
+  ngOnInit() {
+    console.log("achievements", achievements);
+    this.achievements.splice(0, 10);
+  }
 }
